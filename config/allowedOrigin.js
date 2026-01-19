52cee9989d9e3581543093dd8a2ad3633a472dc6
Prajwol <prajwolsubedizzz@gmail.com>
2026-01-19 08:23:01 +0545

    update: config file updated

---
+++ config/allowedOrigin.js
@@ -0,0 +1,11 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'http://localhost:3500',
+    'https://www.dandrepairshop.com',
+    'https://www.google.com',
+    'https://google.com',
+    'https://prajwolsubedi.com.np',
+    'https://www.prajwolsubedi.com.np'
+]
+
+module.exports = allowedOrigins
