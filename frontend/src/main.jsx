4b9b6ff3352d8b65b459eaefda6e26ff538cb571
Prajwol <prajwolsubedizzz@gmail.com>
2026-01-20 09:34:38 +0545

    frontned initial commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { store } from "./app/store.jsx";
+import { Provider } from "react-redux";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>,
+);
